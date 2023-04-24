0fff7c043be2a50192fe54ca3319f487ecd067fc
Diego_dos_Santos_Fernandes <cookieezito@hotmail.com>
2023-04-24 01:57:48 -0400

    1.3

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ import { TouchableOpacity } from 'react-native';
 export default function App() {
   const [tela, setTela] = useState('menu');
   const [jogadorAtual, setJogadorAtual] = useState('');
+  const [jogadorMaquina, setJogadorMaquina] = useState('');
   const [tabuleiro, setTabuleiro] = useState([]);
   const [jogadasRestantes, setJogadasRestantes] = useState(0);
   const [ganhador, setGanhador] = useState('');
 
+  // Configuração do player em cima da matriz"tabuleiro" do jogo
   function iniciarJogo(jogador) {
     setJogadorAtual(jogador);
     setJogadasRestantes(9);
@@ -20,55 +22,72 @@ export default function App() {
     ]);
     setTela('jogo');
   }
-
-  function jogar(linha, coluna) {
-    tabuleiro[linha][coluna] = jogadorAtual;
-    setTabuleiro([...tabuleiro]);
-
-    setJogadorAtual(jogadorAtual === 'X' ? 'O' : 'X')
-
-    verificarGanhador(tabuleiro, linha, coluna);
-
+  function contraMaquina(jogador) {
+    setJogadorAtual(jogador);
+    setJogadasRestantes(9);
+    setTela('jogador');
+    setTabuleiro([
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]);
+    setTela('vscpu');
   }
-
-  function verificarGanhador(tabuleiro, linha, coluna) {
-    // linhas
-    if(tabuleiro[linha][0] !== '' && tabuleiro[linha][0] == tabuleiro[linha][1] && tabuleiro[linha][1] === tabuleiro[linha][2]){
-      return finalizarJogo(tabuleiro[linha][0]);
-    }
-    // coluna
-    if(tabuleiro[0][coluna] !== '' && tabuleiro[0][coluna] === tabuleiro[1][coluna] && tabuleiro[1][coluna] === tabuleiro[2][coluna]){
-      return finalizarJogo(tabuleiro[0][coluna]);
-    }
-    // diagonal 1
-    if(tabuleiro[0][0] !== '' && tabuleiro[0][0] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][2]){
-      return finalizarJogo(tabuleiro[0][0]);
-    }
-    // diagonal 2
-    if(tabuleiro[0][2] !== '' && tabuleiro[0][2] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][0]){
-      return finalizarJogo(tabuleiro[0][2]);
-    }
-    // nenhum ganhador
-    if((jogadasRestantes -1) === 0) {
-      return finalizarJogo('');
-    }
-    // jogo não finalizado
-    setJogadasRestantes((jogadasRestantes - 1));
-  }
-
-  function finalizarJogo(jogador) {
-    setGanhador(jogador);
-    setTela('ganhador');
-  }
-
+  // Script para setar as funções de menu, dentro de jogo e de resultado do jogo.
   switch (tela) {
     case 'menu':
       return getTelaMenu();
     case 'jogo':
       return getTelaJogo();
+    case 'vscpu':
+      return getTelaVsCPU();
     case 'ganhador':
       return getTelaGanhador();
+    case 'jogador':
+      return escolherJogador();
   };
+  // Exibi o tabuleiro do jogo com suas peças
+  function jogar(linha, coluna) {
+    tabuleiro[linha][coluna] = jogadorAtual;
+    setTabuleiro([...tabuleiro]);
+
+    setJogadorAtual(jogadorAtual === 'X' ? 'O' : 'X')
+    setJogadorMaquina(jogadorMaquina === 'X' ? 'O' : 'X')
+
+    verificarGanhador(tabuleiro, linha, coluna);
+
+  }
+  // Verifica se tem vencedor, perdedor ou se empatou
+  function verificarGanhador(tabuleiro, linha, coluna) {
+    // linhas
+    if (tabuleiro[linha][0] !== '' && tabuleiro[linha][0] == tabuleiro[linha][1] && tabuleiro[linha][1] === tabuleiro[linha][2]) {
+      return finalizarJogo(tabuleiro[linha][0]);
+    }
+    // coluna
+    if (tabuleiro[0][coluna] !== '' && tabuleiro[0][coluna] === tabuleiro[1][coluna] && tabuleiro[1][coluna] === tabuleiro[2][coluna]) {
+      return finalizarJogo(tabuleiro[0][coluna]);
+    }
+    // diagonal 1
+    if (tabuleiro[0][0] !== '' && tabuleiro[0][0] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][2]) {
+      return finalizarJogo(tabuleiro[0][0]);
+    }
+    // diagonal 2
+    if (tabuleiro[0][2] !== '' && tabuleiro[0][2] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][0]) {
+      return finalizarJogo(tabuleiro[0][2]);
+    }
+    // nenhum ganhador
+    if ((jogadasRestantes - 1) === 0) {
+      return finalizarJogo('');
+    }
+    // jogo não finalizado
+    setJogadasRestantes((jogadasRestantes - 1));
+  }
+  // Caso a função verificadora de ganhador funcione, o jogo deve finalizar e puxar a função de exibir os resultados
+  function finalizarJogo(jogador) {
+    setGanhador(jogador);
+    setTela('ganhador');
+  }
+  // Exibe o menu do Jogo
   function getTelaMenu() {
     return (
       <View style={styles.container}>
@@ -84,15 +103,41 @@ export default function App() {
             <Text style={styles.jogadorO}>O</Text>
           </TouchableOpacity>
         </View>
+        <TouchableOpacity style={styles.boxMaquina} onPress={() => setTela('jogador')}>
+          <Text style={styles.maquina}>VS Computador</Text>
+        </TouchableOpacity>
       </View>
     );
   }
+  // Exibi menu de escolha de jogador no modo Contra Maquina
+  function escolherJogador() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style='auto' />
+        <Text style={styles.titulo}>Jogo da Velha</Text>
+        <Text style={styles.subtitulo}>selecione seu jogador</Text>
+
+        <View style={styles.inlineItems}>
+          <TouchableOpacity style={styles.boxJogador} onPress={() => contraMaquina('X')}>
+            <Text style={styles.jogadorX}>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.boxJogador} onPress={() => contraMaquina('O')}>
+            <Text style={styles.jogadorO}>O</Text>
+          </TouchableOpacity>
+
+        </View>
+        <TouchableOpacity style={styles.botaoMenu} onPress={() => setTela('menu')}>
+          <Text style={styles.textoBotaoMenu}>Voltar ao Menu</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  // Tela de jogo Contra Player
   function getTelaJogo() {
     return (
       <View style={styles.container}>
         <StatusBar style='auto' />
         <Text style={styles.titulo} >Jogo da Velha</Text>
-
         {
           tabuleiro.map((linha, numeroLinha) => {
             return (
@@ -102,7 +147,7 @@ export default function App() {
                     return (
                       <TouchableOpacity key={numeroColuna} style={styles.boxJogador}
                         onPress={() => jogar(numeroLinha, numeroColuna)} disabled={coluna !== ''}>
-                        <Text style={coluna === 'X' ? styles.boxJogadorX : styles.boxJogadorO}>{coluna}</Text>
+                        <Text style={coluna === 'X' ? styles.jogadorX : styles.jogadorO}>{coluna}</Text>
                       </TouchableOpacity>
                     )
                   })
@@ -118,23 +163,56 @@ export default function App() {
       </View>
     );
   }
+  // Tela de Jogo Contra Maquina
+  function getTelaVsCPU() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style='auto' />
+        <Text style={styles.titulo} >Jogo da Velha</Text>
+        {
+          tabuleiro.map((linha, numeroLinha) => {
+            return (
+              <View key={numeroLinha} style={styles.inlineItems}>
+                {
+                  linha.map((coluna, numeroColuna) => {
+                    return (
+                      <TouchableOpacity key={numeroColuna} style={styles.boxJogador}
+                        onPress={() => jogar(numeroLinha, numeroColuna)} disabled={coluna !== ''}>
+                        <Text style={coluna === 'X' ? styles.jogadorX : styles.jogadorO}>{coluna}</Text>
+                      </TouchableOpacity>
+                    )
+                  })
+                }
+              </View>
+            )
+          })
+        }
+
+        <TouchableOpacity style={styles.botaoMenu} onPress={() => setTela('menu')}>
+          <Text style={styles.textoBotaoMenu}>Voltar ao Menu</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  // Exibe o resultado de quem ganhou o jogo
   function getTelaGanhador() {
     return (
       <View style={styles.container}>
         <StatusBar style='auto' />
         <Text style={styles.titulo}>Jogo da Velha</Text>
-        <Text style={styles.subtitulo}>Resultado Final</Text>
+        <Text style={styles.subtituloFinal}>Resultado Final:</Text>
 
         {
-          ganhador === '' && <Text style={styles.ganhador}>Nenhum Ganhador</Text>
+          ganhador === '' &&
+          <Text style={styles.ganhador}>Empatou</Text>
         }
         {
           ganhador !== '' &&
           <>
-            <Text style={styles.ganhador}>Ganhador</Text>
             <View style={styles.boxJogador}>
-              <Text style={ganhador === 'X' ? styles.boxJogadorX : styles.boxJogadorO}>{coluna}</Text>
+              <Text style={ganhador === 'X' ? styles.jogadorX : styles.jogadorO}>{ganhador}</Text>
             </View>
+            <Text style={styles.ganhador}>GANHOU</Text>
           </>
         }
 
@@ -154,27 +232,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   titulo: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: '#20232a',
-    borderRadius: 6,
-    backgroundColor: 'rgb(210,10,250)',
     textAlign: 'center',
     fontSize: 40,
     fontWeight: 'bold',
-    color: 'white',
   },
   subtitulo: {
-    width: 220,
     marginTop: 4,
     paddingVertical: 2,
     fontSize: 18,
     fontWeight: 'bold',
     color: 'rgb(100,100,100)',
   },
+  subtituloFinal: {
+    marginTop: 4,
+    paddingVertical: 2,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+  },
   boxJogador: {
     width: 80,
+    height: 80,
+    backgroundColor: "#ddd",
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5
+  },
+  boxMaquina: {
+    width: 170,
     height: 80,
     backgroundColor: "#ddd",
     alignItems: 'center',
@@ -189,6 +274,10 @@ const styles = StyleSheet.create({
     fontSize: 40,
     color: '#da3f3f'
   },
+  maquina: {
+    fontSize: 18,
+    color: '#ff8000'
+  },
   inlineItems: {
     flexDirection: 'row'
   },
@@ -199,7 +288,7 @@ const styles = StyleSheet.create({
     color: '#4e6fe4'
   },
   ganhador: {
-    fontSize: 30,
+    fontSize: 28,
     fontWeight: 'bold',
     color: '#333'
   },
